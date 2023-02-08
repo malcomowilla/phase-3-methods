97b86f2c2fc22b91e7fd68f4e2fd08ad53b88ac3
malcomowilla <owillamalcom@gmail.com>
2023-02-08 09:50:40 +0300

    methods

--- methods.rb
+++ methods.rb
@@ -1,1 +1,93 @@
 # Your code here!
+
+
+
+# def my_method(param)
+#     puts "Running my_method"
+#     param  + 1
+    
+# end
+
+# function sayHi(name = "friend") {
+#   console.log(`Hi there, ${name}!`);
+# }
+
+# sayHi();
+# // => "Hi there, friend!"
+# sayHi("Sunny");
+# // => "Hi there, Sunny!"
+
+
+#  def say_hi(name = "Rubyist")
+#     puts "hi there, #{name}!"
+#  end
+
+#  say_hi
+#  =>"hi there, Rubyist!"
+
+#  say_hi "sunny"
+#  =>"hi there, Sunny"
+
+
+# def add_and_log(num1, num2)
+# puts num1 + num2
+# end
+
+# def add_and_return(num1, num2)
+#     return num1 + num2
+# end
+
+# sum1 = add_and_log(2, 2)
+# => nil
+# sum2 = add_and_return(2, 2)
+
+def greet_programmer
+    puts "Hello, programmer!"
+    
+end
+
+greet_programmer
+
+
+
+
+
+
+def greet name
+
+    puts "Hello, #{name}!"
+    
+end
+
+greet "Naureen"
+
+def greet_with_default name="Hello, programmer!"
+    puts "Hello, #{name}!"
+
+   
+end
+
+
+greet_with_default "Naureen"
+
+def add num1, num2
+    num1 + num2
+    
+end
+
+add 2, 5
+
+
+def halve num1
+  if num1.class != Integer
+        return nil
+    
+end
+
+
+ num1 / 2
+end
+
+halve 2
+
+
